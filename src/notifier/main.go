ccebd48a921daf72b3a91b76a6740967a088242e
Ricardo Salinas <ricardo@salinas.id.au>
2021-09-17 22:04:13 +1000

    (fix) Typo

--- src/notifier/main.go
+++ src/notifier/main.go
@@ -102,7 +102,7 @@ func hash(jsondata []byte, state State) string {
 }
 
 func lastest_data(state State) []byte {
-	resp, err := http.Get("https://discover.data.vic.gov.au/api/3/action/datastore_search?resource_id=afb52611-6061-4a2b-9110-74c920bede77?q=" + state.Suburb)
+	resp, err := http.Get("https://discover.data.vic.gov.au/api/3/action/datastore_search?resource_id=afb52611-6061-4a2b-9110-74c920bede77&q=" + state.Suburb)
 	if err != nil {
 		log.Fatal(err)
 	}
